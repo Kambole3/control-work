ba0f4e21b57e5048451e3246bfa03de8d3faab98
User <user@MacBook-Air-2.local>
2024-11-13 12:41:52 +0300

    first commit

---
+++ src/mock/expenses.js
@@ -0,0 +1,8 @@
+export const expense = [
+    
+    { id: 1, type: "food", name: "cow meat and fries",value: "1000 rubles" },
+    { id: 2, type: "transport", name: "flight",value: "100000 rubles " },
+    { id: 3, type: "entertainment", name: "party",value: "5000 rubles " },
+    { id: 4, type: "others", name: "safari", value: "7000 rubles" }
+]
+  
